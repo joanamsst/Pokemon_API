96c4abfd3b8113c6f83c38aa502d3c7f7632d930
Joana Trindade <joana.trindade@ceiia.com>
2026-05-15 17:15:52 +0100

    delete unused files

--- pokedex/src/components/pokemon/PokemonMiniCard.tsx
+++
@@ -1,9 +0,0 @@
-import { Text, View } from "react-native";
-
-export default function PokemonMiniCard() {
-  return (
-    <View>
-      <Text>Pokemon Mini Card</Text>
-    </View>
-  );
-}
--- pokedex/src/components/pokemon/TypeBadge.tsx
+++
@@ -1,9 +0,0 @@
-import { Text, View } from "react-native";
-
-export default function TypeBadge() {
-  return (
-    <View>
-      <Text>Type Badge</Text>
-    </View>
-  );
-}
